3050c9762668368873cb9e2b0e0d3307247a91cd
MMz <mmzsource@users.noreply.github.com>
2020-07-04 11:05:51 +0200

    Make the home page easier to read

--- site/index.clj
+++ site/index.clj
@@ -3,24 +3,22 @@
 (defn body []
   [:body
 
-   [:p {:class "center"}
-    "When you see a real artist, one who is really good at it, then you know
-    right away that there’s such a thing as an art of motorcycle
-    maintenance."]
-
    [:blockquote
-    "Art is anything you can do well." [:br]
-    "Anything where there are options for doing it well or poorly." [:br]
+    "Art is anything you can do well," [:br]
+    "anything where there are options for doing it well or poorly." [:br]
     "~ Robert Pirsig"]
 
    [:p {:class "center"}
+    "When you see a real artist, one who is really good at it, then you know
+    right away that there’s such a thing as an art of motorcycle
+    maintenance." [:br]
+    "Art is anything you can do well, anything where there are options for doing
+    it well or poorly." [:br]
     "And there are very few things in this world that don’t have options for
     doing it well. You can make an art out of anything. And I think that making
-    an art out of your technical life is solving the problem of technology."]
-
-   [:p {:class "center"}
+    an art out of your technical life is solving the problem of technology." [:br]
     "Quotes from Robert Pirsig, author of 'Zen and the art of motorcycle
-    maintenance'"]
+    maintenance' and 'Lila'."]
 
    [:blockquote
     "Live clean, let your works be seen." [:br]
