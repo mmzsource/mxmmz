f2e1c5714f156c2fb54a1e16e359e9a169eb2d64
MMz <mmzsource@users.noreply.github.com>
2020-07-06 06:17:01 +0200

    update matrix rain post

--- site/blog/rain.clj
+++ site/blog/rain.clj
@@ -22,31 +22,34 @@
 
    [:h2 "Concepts"]
    [:p "Instead of explaining the source code in detail, I thought it would be
-   better to explain the concepts behind my design. In my view, matrix rain is
-   basically a grid of randomly changing symbols, where the drops of rain are
-   'overlayed' on that grid by manipulating the colors of the symbols on it."]
+   better to explain the concepts behind my design. In essence, I've created a
+   grid of randomly changing symbols, where the drops of rain are 'overlayed' on
+   that grid by manipulating the colors of the symbols."]
 
-   [:img {:src "./matrixrain-img/matrix.png"}]
+   [:img {:src "./matrixrain-img/matrix.png"
+          :alt "A grid of symbols"}]
 
    [:div {:class "imgtxt-container"}
-    [:img {:class "container-img" :src "./matrixrain-img/raindrop-short.png"}]
+    [:img {:class "container-img"
+           :src   "./matrixrain-img/raindrop-short.png"
+           :alt   "raindrop coloring"}]
     [:div {:class "container-txt"}
 
      [:h2 "Matrix Grid"]
-     [:p "First you fill a grid by filling the grid cells with randomly picked
-     symbols. Now we only have to randomly change symbols on the grid in a rate
-     that looks about right and we're already done with the grid. On to the
-     raindrops."]
+     [:p "First I've filled the cells of the grid with randomly picked symbols.
+     After making sure the symbols on the grid change in a rate that looks about
+     right, the grid is already done. On to the raindrops."]
 
      [:h2 "Raindrops"]
-     [:p "Now the grid is in place, the raindrops need to get the right color.
-     White for the head, the two symbols behind that a little bit gray(er) and
+     [:p "Now the grid is in place, the raindrops need to get the right color:
+     white for the head, the two symbols behind that a little bit gray(er) and
      then a green, descending gradient, followed by completely invisible grid
-     symbols ... until the next drop of rain runs over them!"]
+     symbols. After adding some randomization in raindrop length and raindrop
+     speed, the raindrops are done as well."]
 
      [:h2 "Glueing it together"]
      [:p "The " (html/a "http://quil.info" "Quil library") " will take care of
-     the rendering on canvas, as long as we tell it what function to call to
+     the rendering on canvas. It only needs to know what function to call to
      'update' the matrix state and what function to call to 'draw' the current
      matrix state. The comment blocks in my code mark the different
      responsibilities of the code sections: definitions of some constants and
@@ -60,10 +63,10 @@
      design is quite easy to understand and implement and the result looks
      pretty similar to the matrix rain as seen in the Matrix movies. According
      to " (html/a "https://github.com/AlDanial/cloc" "cloc") " it took me 102
-     lines of ClojureScript to get this matrix rain on my screen. I hope it
-     inspires you to create something similar in your own language of choice. Be
-     sure to drop me a link on " (html/twitter) " once it's up and running on a
-     website."]
+     lines of ClojureScript to get this matrix rain running on my screen. I hope
+     it inspires you to create something similar in your own language of choice.
+     Be sure to drop me a link on " (html/twitter) " once it's up and running on
+     your website."]
 
      [:p "Happy coding!"]]]])
 
